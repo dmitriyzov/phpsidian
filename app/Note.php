a656d77925ee957cdc9aa3361245cd6d716188d4
dmitriyklyuzov <dmitriy.klyuzov@gmail.com>
2026-03-07 14:56:33 -0500

    removed getters and setters from Note and made it a readonly class instead

--- app/Note.php
+++ app/Note.php
@@ -1,18 +1,10 @@
 <?php
 declare(strict_types=1);
 
-class Note
+readonly class Note
 {
     public function __construct(
-        private string $title,
-        private string $content
+        public string $title,
+        public string $content
     ) {}
-
-    public function getTitle(): string {
-        return $this->title;
-    }
-
-    public function setTitle(string $title): void {
-        $this->title = $title;
-    }
 }
